c5acd7a39669d039c171fe1d536c8f16354bf2b6
Syssero <135151101+Syssero@users.noreply.github.com>
2025-05-26 14:40:51 -0400

    removed listen port from app.js due to error message

--- app.js
+++ app.js
@@ -38,9 +38,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const port = process.env.PORT || 3006;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-
 module.exports = app;
